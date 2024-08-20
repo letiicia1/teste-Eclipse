7f994bd191b278983c719f9d000a5616c01c936b
Letícia <111805836+letiicia1@users.noreply.github.com>
2024-08-20 09:17:53 -0300

    Add files via upload

---
+++ Teste2/src/SomarDoisNumeros.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+class SomarDoisNumeros {
+
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main (String args[]){
+		//Declaracao de variaveis
+		int num1, num2, soma;
+		//Leituras
+		System.out.print("Digite um número: ");
+		num1 = sc.nextInt();
+		System.out.print("Digite outro número: ");
+		num2 = sc.nextInt();
+		//Somar
+		soma = num1 + num2;
+		//Mostrar na tela
+		System.out.println(" ");
+		System.out.println("Soma: " + soma);
+		}
+}
